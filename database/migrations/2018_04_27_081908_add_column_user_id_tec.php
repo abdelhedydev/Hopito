3afa4e130335c02311f3d9a6dde2792af0bd964d
abdelhedi <abdelhedihlel@gmail.com>
2018-05-18 11:37:27 +0100

    coorect migrations

--- database/migrations/2018_04_27_081908_add_column_user_id_tec.php
+++ database/migrations/2018_04_27_081908_add_column_user_id_tec.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnUserId extends Migration
+class AddColumnUserIdTec extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddColumnUserId extends Migration
      */
     public function up()
     {
-        Schema::table('secretaires', function (Blueprint $table) {
+        Schema::table('techniciens', function (Blueprint $table) {
           $table->integer('user_id')->unsigned()->after('id');
          $table->foreign('user_id')->references('id')->on('users');
         });
@@ -26,7 +26,7 @@ class AddColumnUserId extends Migration
      */
     public function down()
     {
-        Schema::table('secretaires', function (Blueprint $table) {
+        Schema::table('techniciens', function (Blueprint $table) {
           $table->dropForeign(['user_id']);
          $table->dropColumn('user_id');
         });
